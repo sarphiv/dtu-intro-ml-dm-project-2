89aa90d1e77a3c6a11aa56425df6c30694111f8f
sarphiv <32750711+sarphiv@users.noreply.github.com>
2021-10-26 19:48:27 +0200

    Fixed data format of example code to conform to agreed upon spec

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,8 +1,11 @@
 from validation.validation_model import ValidationModel
 import sklearn.linear_model as lm
+import numpy as np
 
 
 class LinearRegression(ValidationModel):
     def train_predict(self, train_features, train_labels, test_features):
-        m = lm.LinearRegression(fit_intercept=True).fit(train_features, train_labels)
-        return m.predict(test_features)
+        m = lm.LinearRegression(fit_intercept=True).fit(
+            np.expand_dims(train_features, 1), 
+            train_labels)
+        return m.predict(np.expand_dims(test_features, 1))
