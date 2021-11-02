a38be224e8ba4fb410ac35fabf6b9887ac294874
sarphiv <32750711+sarphiv@users.noreply.github.com>
2021-11-02 15:58:07 +0100

    CalleRC: Added linear regression

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,11 +1,14 @@
 from validation.validation_model import ValidationModel
-import sklearn.linear_model as lm
+from sklearn.linear_model import Ridge
 import numpy as np
 
 
 class LinearRegression(ValidationModel):
+    def __init__(self, lamb):
+        self.lamb = lamb
+    
     def train_predict(self, train_features, train_labels, test_features):
-        m = lm.LinearRegression(fit_intercept=True).fit(
-            np.expand_dims(train_features, 1), 
-            train_labels)
-        return m.predict(np.expand_dims(test_features, 1))
+        m = Ridge(alpha=self.lamb, fit_intercept=True)
+        m.fit(train_features, train_labels)
+        
+        return m.predict(test_features, 1)
